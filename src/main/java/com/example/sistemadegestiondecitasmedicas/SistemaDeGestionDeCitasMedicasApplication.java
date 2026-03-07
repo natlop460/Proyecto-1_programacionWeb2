3cc2f49c01406d37e1a0aeccba44e42585036b64
Nathalie lopez <natalie.lopez12@hotmail.com>
2026-03-06 22:03:41 -0600

    Commit inicial

---
+++ src/main/java/com/example/sistemadegestiondecitasmedicas/SistemaDeGestionDeCitasMedicasApplication.java
@@ -0,0 +1,13 @@
+package com.example.sistemadegestiondecitasmedicas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeGestionDeCitasMedicasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SistemaDeGestionDeCitasMedicasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sistemadegestiondecitasmedicas/SistemaDeGestionDeCitasMedicasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sistemadegestiondecitasmedicas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeGestionDeCitasMedicasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
